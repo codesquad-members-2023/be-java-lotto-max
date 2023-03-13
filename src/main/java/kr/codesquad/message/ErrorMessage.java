5aa6a6be1ec01b421b8715d3f5c0b75b7e839936
jinny-l <jinny.l.dev@gmail.com>
2023-03-13 16:45:12 +0900

    ✨feat: 구입 금액(Money.java) 기능 구현
    * Money 클래스 구현
    * 유효성 검증
      - 1000원 미만으로 입력했을 때 예외처리
      - 1000원 단위로 입력하지 않았을 때 예외처리

---
+++ src/main/java/kr/codesquad/message/ErrorMessage.java
@@ -0,0 +1,24 @@
+package kr.codesquad.message;
+
+import kr.codesquad.model.Money;
+
+public enum ErrorMessage {
+
+    EMPTY_INPUT_ERROR("입력 값이 비어있습니다."),
+    INVALID_INPUT_TYPE("로또 구입 금액은 숫자만 입력 가능합니다."),
+    INVALID_MONEY(String.format("로또 구입 금액은 %d원 단위로 입력 가능합니다.", Money.LOTTO_PRICE_UNIT));
+
+    private static final String ERROR = "[ERROR] ";
+
+    private final String message;
+
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return ERROR + message;
+    }
+}
