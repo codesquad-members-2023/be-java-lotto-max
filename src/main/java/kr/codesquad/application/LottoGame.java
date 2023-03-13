4247f1abead6049a09a1b89e83fc0cb74b450e85
JJONSOO <pjs970616@naver.com>
2023-03-13 18:06:03 +0900

    refactor : LottoGame이 너무 많은 일을 하므로 로또를 구매하는 클래스를 생성, 당첨 결과 계산을 calculator 클래스가 하도록 수정

--- src/main/java/kr/codesquad/application/LottoGame.java
+++ src/main/java/kr/codesquad/application/LottoGame.java
@@ -1,69 +1,41 @@
 package kr.codesquad.application;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 import kr.codesquad.domain.Lotto;
 import kr.codesquad.domain.LottoCalculator;
+import kr.codesquad.domain.LottoMarket;
 import kr.codesquad.domain.LottoResult;
-import kr.codesquad.generator.LottoGenerator;
 import kr.codesquad.util.Validation;
 import kr.codesquad.view.View;
 
 public class LottoGame {
-	LottoGenerator lottoGenerator;
+	LottoMarket lottoMarket;
 	List<Lotto> purchasedLotto;
 	LottoCalculator lottoCalculator;
 	Lotto winningLotto;
-	Integer bonusNumber;
 	LottoResult lottoResult;
 
 	public LottoGame(){
-		lottoGenerator = new LottoGenerator();
 		lottoCalculator = new LottoCalculator();
-		lottoResult = new LottoResult();
-		buyLotto();
-		getWinningLottoNumber();
+		lottoMarket = new LottoMarket();
+		purchasedLotto = lottoMarket.buyLotto();
 		printPurchasedLotto();
+		lottoResult = lottoCalculator.calculateLottoAndAddResult(purchasedLotto, getWinningLotto(), getValidateBonusNumber());
+
 	}
 
-	public void buyLotto(){
-		Integer countLotto = getValidateCountLotto();
-		purchasedLotto = getPurchasedManualLotto();
-		purchasedLotto.addAll(lottoGenerator.purchaseLotto(countLotto - purchasedLotto.size()));
+	public Lotto getWinningLotto(){
+		return new Lotto(getValidateLotto("당첨 번호를 입력해주세요\n"));
 	}
 
-	public void getWinningLottoNumber(){
-		winningLotto = new Lotto(getValidateLotto("당첨 번호를 입력해주세요\n"));
-		bonusNumber = getValidateBonusNumber();
-	}
 	public void printPurchasedLotto(){
 		View.printLottoCount(purchasedLotto.size());
-		purchasedLotto.stream().forEach(lotto -> View.printLotto(lotto.toString()));
-	}
-
-	public List<Lotto> getPurchasedManualLotto(){
-		Integer manualLottoCount = View.manualLottoCount();
-		return IntStream.range(0, manualLottoCount)
-			.mapToObj(i -> new Lotto(getValidateLotto("")))
-			.collect(Collectors.toList());
+		purchasedLotto.forEach(lotto -> View.printLotto(lotto.toString()));
 	}
 
 	public void run(){
-		calculateLottoAndAddResult();
 		lottoResult.printResults();
 	}
-
-	public void calculateLottoAndAddResult(){
-		purchasedLotto.stream()
-			.filter(lotto -> lotto.countSameNumbers(winningLotto) >= 3)
-			.map(lotto -> lottoCalculator.calculateLotto(lotto, winningLotto, bonusNumber))
-			.forEach(lottoResult::addResult);
-	}
-
-
 
 	public Integer getValidateBonusNumber(){
 		Integer bonusNumber =0;
@@ -77,7 +49,8 @@ public class LottoGame {
 		}
 		return bonusNumber;
 	}
-	public String getValidateLotto(String outputComment){
+
+	public static String getValidateLotto(String outputComment){
 		String winningLotto = View.getLotto(outputComment);
 		try{
 			Validation.validateInputLotto(winningLotto);
@@ -89,15 +62,5 @@ public class LottoGame {
 		}
 		return winningLotto;
 	}
-	public Integer getValidateCountLotto(){
-		Integer count =0;
-		try{
-			count = View.countLotto();
-			Validation.validateCountLotto(count);
-		}catch (Exception e){
-			View.printErrorMessage(e.getMessage());
-			getValidateCountLotto();
-		}
-		return count/1000;
-	}
+
 }
