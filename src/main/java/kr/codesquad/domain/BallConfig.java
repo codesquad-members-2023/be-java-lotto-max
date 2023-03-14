6fd2475836174b449e85ced0ff090a9bc4eeedfe
ALbertIM0427 <guang7226961@naver.com>
2023-03-14 14:26:18 +0900

    BallConfig private 생성자 추가

--- src/main/java/kr/codesquad/domain/BallConfig.java
+++ src/main/java/kr/codesquad/domain/BallConfig.java
@@ -6,4 +6,8 @@ public class BallConfig {
 	public static final int MAX_BALL_NUMBER = 45;
 	public static final int MAX_BALL_NUMBER_ADD_ONE = MAX_BALL_NUMBER + 1;
 	public static final int TICKET_PRICE = 1000;
+
+	private BallConfig() {
+
+	}
 }
--- src/main/java/kr/codesquad/view/OutputManager.java
+++ src/main/java/kr/codesquad/view/OutputManager.java
@@ -7,7 +7,7 @@ import kr.codesquad.domain.Ticket;
 
 public class OutputManager {
 	public void printTickets(int quantity, List<Ticket> tickets) {
-		System.out.println(quantity+"개를 구매했습니다.");
+		System.out.println(quantity + "개를 구매했습니다.");
 		tickets.forEach(ticket -> System.out.println(ticket.getNumbers()));
 	}
 
