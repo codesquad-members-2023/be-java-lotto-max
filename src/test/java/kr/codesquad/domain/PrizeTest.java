8664e09aaf8f01d798df6f1dc13253adfcd913ab
ALbertIM0427 <guang7226961@naver.com>
2023-03-14 14:04:01 +0900

    Prize클래스 테스트 추가

---
+++ src/test/java/kr/codesquad/domain/PrizeTest.java
@@ -0,0 +1,23 @@
+package kr.codesquad.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class PrizeTest {
+
+	@DisplayName("MathCount 입력하면 대응한 Prize 타입을 만들어준다")
+	@ParameterizedTest
+	@CsvSource({"2,NONE", "0,NONE", "3,FOURTH", "4,THIRD", "5,SECOND", "6,FIRST"})
+	void createByMatchCount(int match, Prize prize) {
+		assertThat(Prize.createByMatchCount(match)).isEqualTo(prize);
+	}
+
+	@Test
+	void getWinningPrize() {
+		assertThat(Prize.getWinningPrize()).containsOnly(Prize.FIRST, Prize.SECOND, Prize.THIRD, Prize.FOURTH);
+	}
+}
