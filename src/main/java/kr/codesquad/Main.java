29d854c2f85a15f7dff3290c2843ff0bfc7f8586
JIHYE <jdh324@naver.com>
2023-03-14 11:30:16 +0900

    주요 로직이 있는 로또 게임 클래스 생성

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,16 +1,12 @@
 package kr.codesquad;
 
-import kr.codesquad.view.Input;
+import kr.codesquad.domain.LottoGame;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Input input = new Input();
-        String purchaseAmount = input.input(br, 0);
-        String luckyNumber = input.input(br, 1);
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.start();
     }
 }
---
+++ src/main/java/kr/codesquad/domain/LottoGame.java
@@ -0,0 +1,8 @@
+package kr.codesquad.domain;
+
+import java.io.IOException;
+
+public class LottoGame {
+    public void start() throws IOException {
+    }
+}
