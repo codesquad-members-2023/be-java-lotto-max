c640daa6c4e7a541a7e642e8067d982482adc1ad
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-03-15 15:49:08 +0900

    refactor: 로또 당첨 조건 리팩토링에 따른 변경

--- src/main/java/kr/codesquad/domain/PrizeCondition.java
+++ src/main/java/kr/codesquad/domain/PrizeCondition.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 
 public abstract class PrizeCondition {
 
-    protected static final int NOPRIZE = 0;
-
-    public PrizeCondition(){
-
-    }
+    protected static final int NO_PRIZE = 0;
 
 
      abstract protected int returnPrize(ArrayList<Integer> winningNumbers, ArrayList<Integer> ticketNumber);
-     abstract protected boolean checkCondition(ArrayList<Integer> winningNumbers, ArrayList<Integer> ticketNumber);
+
+    abstract protected boolean checkCondition(ArrayList<Integer> winningNumbers, ArrayList<Integer> ticketNumber);
+
+     @Override
+     public abstract String toString();
+
 
 }
 
--- src/main/java/kr/codesquad/domain/PrizeConditionList.java
+++ src/main/java/kr/codesquad/domain/PrizeConditionList.java
@@ -2,27 +2,42 @@ package main.java.kr.codesquad.domain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-import main.java.kr.codesquad.domain.PrizeConditions.FirstPrizeCondition;
-import main.java.kr.codesquad.domain.PrizeConditions.FouthPrizeCondition;
-import main.java.kr.codesquad.domain.PrizeConditions.SecondPrizeCondition;
-import main.java.kr.codesquad.domain.PrizeConditions.ThirdPrizeCondition;
+import main.java.kr.codesquad.domain.PrizeConditions.MatchingNumberCountCondition;
 
 public class PrizeConditionList {
-    private ArrayList<PrizeCondition> prizeConditions = new ArrayList<>(Arrays.asList(
-            new FirstPrizeCondition(),
-            new SecondPrizeCondition(),
-            new ThirdPrizeCondition(),
-            new FouthPrizeCondition()
-    ));
+    private HashMap<PrizeCondition, Integer> prizeConditionsAndCount = new HashMap<>();
+    {
+        prizeConditionsAndCount.put(new MatchingNumberCountCondition(PrizeType.FIRST_PRIZE), 0);
+        prizeConditionsAndCount.put(new MatchingNumberCountCondition(PrizeType.SECOND_PRIZE), 0);
+        prizeConditionsAndCount.put(new MatchingNumberCountCondition(PrizeType.THIRD_PRIZE), 0);
+        prizeConditionsAndCount.put(new MatchingNumberCountCondition(PrizeType.FOURTH_PRIZE), 0);
+    }
 
-    public int retunPrize(ArrayList<Integer> winningNumbers, ArrayList<Integer> ticketNumbers) {
+
+    public int returnPrize(ArrayList<Integer> winningNumbers, ArrayList<Integer> ticketNumbers) {
         int wonAmount = 0;
-        for (PrizeCondition prizeCondition : prizeConditions) {
-            wonAmount += prizeCondition.returnPrize(winningNumbers, ticketNumbers);
+        for (Map.Entry<PrizeCondition, Integer> entry : prizeConditionsAndCount.entrySet()) {
+            int prize = entry.getKey().returnPrize(winningNumbers, ticketNumbers);
+            if (prize > 0) {
+                wonAmount += prize;
+                entry.setValue(entry.getValue() + 1);
+            }
         }
         return wonAmount;
     }
 
 
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<PrizeCondition, Integer> entry : prizeConditionsAndCount.entrySet()) {
+            int count = entry.getValue();
+            sb.append(entry.getKey().toString());
+            sb.append(count).append("개\n");
+        }
+        return sb.toString();
+    }
 }
