072ab54cc7e7412354b5611dc9fba9250fe709ff
JIHYE <jdh324@naver.com>
2023-03-15 14:45:56 +0900

    numbers 어레이리스트 지역변수로 변경

--- src/main/java/kr/codesquad/domain/Lotto.java
+++ src/main/java/kr/codesquad/domain/Lotto.java
@@ -1,17 +1,16 @@
 package kr.codesquad.domain;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Lotto {
     private final ArrayList<Integer> lotto;
-    private final ArrayList<Integer> numbers = (ArrayList<Integer>) IntStream.rangeClosed(1, 45)
-            .boxed().collect(Collectors.toList());
 
     public Lotto() {
+        ArrayList<Integer> numbers = (ArrayList<Integer>) IntStream.rangeClosed(1, 45)
+                .boxed().collect(Collectors.toList());
         Collections.shuffle(numbers);
         lotto = (ArrayList<Integer>) numbers.stream().limit(6).collect(Collectors.toList());
         Collections.sort(lotto);
