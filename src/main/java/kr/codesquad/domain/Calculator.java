dbd94bdd499bae82cd9756a514292056252fd17e
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-03-15 15:46:56 +0900

    chore: 필요없는 것 삭제

--- src/main/java/kr/codesquad/domain/Calculator.java
+++
@@ -1,9 +0,0 @@
-package main.java.kr.codesquad.domain;
-
-public class Calculator {
-
-
-    public int division(int purchaseAmount,int ticketPrice) {
-        return purchaseAmount/ticketPrice;
-    }
-}
