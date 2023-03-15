2013ecdf036338adcbc5a66be9fb39a76e34e21a
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-03-15 10:04:27 +0900

    fix: 로또 번호 generator 변경

--- src/main/java/kr/codesquad/domain/LottoNumberGenerator.java
+++ src/main/java/kr/codesquad/domain/LottoNumberGenerator.java
@@ -1,21 +1,37 @@
 package main.java.kr.codesquad.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoNumberGenerator {
 
-    private Random random = new Random();
+    private static final int MIN = 1;
+    private static final int MAX = 45;
 
-    private final int max = 50;
+    private static final int NUMBER_OF_LOTTERY_NUMBERS = 6;
 
-    private final int numberLength = 6;
+    public ArrayList<Integer> generateLottoNumbers() {
+        List<Integer> allNumbers = generateShuffledNumbersList();
 
-    public ArrayList<Integer> createLottoNumber() {
-        ArrayList<Integer> lottoNumbers = new ArrayList<>();
-        for (int i = 0; i < numberLength; i++){
-            lottoNumbers.add(this.random.nextInt(max + 1));
-        }
-        return lottoNumbers;
+        // 6개 뽑아오기
+        ArrayList<Integer> lottoNumber = new ArrayList<>(allNumbers.subList(0, NUMBER_OF_LOTTERY_NUMBERS));
+
+        // 오름차순 정렬
+        Collections.sort(lottoNumber);
+        return lottoNumber;
+    }
+
+    private List<Integer> generateShuffledNumbersList() {
+        List<Integer> allNumbers = IntStream.rangeClosed(MIN, MAX)
+                .boxed()
+                .collect(Collectors.toList());
+
+        // 리스트 섞어주기
+        Collections.shuffle(allNumbers);
+        return allNumbers;
     }
 }
