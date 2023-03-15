297b955ce0a3987f18500a99a103b7e654627ad5
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-03-15 23:13:43 +0900

    feat: 당첨 결과를 분석해서 당첨 개수와 금액을 출력해주는 printWinningStatistics()를 구현
    1등~4등 상금을 상수로 선언하고 당첨 결과의 당첨 개수에 따른 금액을 합쳐서 수익을 출력해준다.

--- src/main/java/kr/codesquad/domain/LottoGame.java
+++ src/main/java/kr/codesquad/domain/LottoGame.java
@@ -3,10 +3,15 @@ package kr.codesquad.domain;
 import kr.codesquad.view.Console;
 
 import java.util.List;
+import java.util.Map;
 
 public class LottoGame {
 
     private final int LOTTO_PRICE = 1000;
+    private final int FOURTH_PRIZE = 5000;
+    private final int THIRD_PRIZE = 50000;
+    private final int SECOND_PRIZE = 1500000;
+    private final int FIRST_PRIZE = 2000000000;
 
     private Console console;
     private Lotto lotto;
@@ -15,15 +20,34 @@ public class LottoGame {
     public void play() {
         console = new Console();
 
+        // 로또 구매
         money = console.inputMoney();
         buyLotto(money);
-
+        // 당첨 번호 입력
         List<Integer> winningNumbers = console.inputWinningNumbers();
+        // 당첨 결과 확인
+        Map<Integer, Integer> winningResult = lotto.checkWinningResult(winningNumbers);
+        printWinningStatistics(winningResult);
+
     }
 
     public void buyLotto(int money) {
         int countOfLotto = money / LOTTO_PRICE;
         lotto = new Lotto(countOfLotto);
         console.printBuyLotto(countOfLotto, lotto.toString());
+    }
+
+    public void printWinningStatistics(Map<Integer, Integer> winningResult) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("당첨 통계\n---------------------------\n");
+        sb.append("3개 일치 (" + FOURTH_PRIZE + "원) - " + winningResult.get(3) + "개\n");
+        sb.append("4개 일치 (" + THIRD_PRIZE + "원) - " + winningResult.get(4) + "개\n");
+        sb.append("5개 일치 (" + SECOND_PRIZE + "원) - " + winningResult.get(5) + "개\n");
+        sb.append("6개 일치 (" + FIRST_PRIZE + "원) - " + winningResult.get(6) + "개\n");
+
+        long totalPrizeMoney = FOURTH_PRIZE * winningResult.get(3) + THIRD_PRIZE * winningResult.get(4)
+                + SECOND_PRIZE * winningResult.get(5) + FIRST_PRIZE * winningResult.get(6);
+        sb.append("총 수익은 " + totalPrizeMoney + "원 입니다.\n");
+        System.out.println(sb);
     }
 }
