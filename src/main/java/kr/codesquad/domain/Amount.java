86f54b62d4438f6a5914152f4dac23f50ab4637c
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-03-15 15:45:58 +0900

    refactor: 계산기 클래스 삭제

--- src/main/java/kr/codesquad/domain/Amount.java
+++ src/main/java/kr/codesquad/domain/Amount.java
@@ -1,18 +1,16 @@
 package main.java.kr.codesquad.domain;
 
 public class Amount {
-    private final static int TicketPrice = 1_000;
+    private final static int TICKET_PRICE = 1_000;
     private final int purchaseAmount;
-
-    Calculator calculator = new Calculator();
 
     public Amount(String inputAmount) {
         AmountValidator amountValidator = new AmountValidator();
-        this.purchaseAmount = amountValidator.validateAmount(TicketPrice, inputAmount);
+        this.purchaseAmount = amountValidator.validateAmount(TICKET_PRICE, inputAmount);
     }
 
     public int calculateTicketCount() {
-        return this.calculator.division(purchaseAmount, TicketPrice);
+        return purchaseAmount/TICKET_PRICE;
     }
 
     public double calculateReturnRate(int wonAmount) {
