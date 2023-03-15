bbf635d4e377031a3066690aa9a6933a48b03fe4
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-03-15 10:02:01 +0900

    fix: 수익률 계산 메소드 수정
    수익률이 잘못 계산 되고 있었음

--- src/main/java/kr/codesquad/domain/Amount.java
+++ src/main/java/kr/codesquad/domain/Amount.java
@@ -2,16 +2,22 @@ package main.java.kr.codesquad.domain;
 
 public class Amount {
     private final static int TicketPrice = 1_000;
-    private int purchaseAmount;
+    private final int purchaseAmount;
 
     Calculator calculator = new Calculator();
 
-    public Amount(String inputAmount){
+    public Amount(String inputAmount) {
         AmountValidator amountValidator = new AmountValidator();
         this.purchaseAmount = amountValidator.validateAmount(TicketPrice, inputAmount);
     }
 
-    public int calculateTicketCount(){
+    public int calculateTicketCount() {
         return this.calculator.division(purchaseAmount, TicketPrice);
+    }
+
+    public double calculateReturnRate(int wonAmount) {
+        double a = (((double)  wonAmount - this.purchaseAmount ) / (double) this.purchaseAmount) * 100;
+        return a;
+
     }
 }
