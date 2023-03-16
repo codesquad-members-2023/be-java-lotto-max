b2c722af579be90026c243fc8e17cb743fe5b5f5
ALbertIM0427 <guang7226961@naver.com>
2023-03-16 12:29:49 +0900

    InputManager의 Validator를 기능을 모두 InputManagerValidator로 클래스 분리.

--- src/main/java/kr/codesquad/view/InputManager.java
+++ src/main/java/kr/codesquad/view/InputManager.java
@@ -1,16 +1,15 @@
 package kr.codesquad.view;
 
 import static kr.codesquad.domain.BallConfig.*;
+import static kr.codesquad.view.InputManagerValidator.*;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import kr.codesquad.domain.Ball;
-import kr.codesquad.domain.LottoBalls;
 import kr.codesquad.domain.Money;
 import kr.codesquad.domain.WinningNumbers;
 
@@ -18,13 +17,6 @@ public class InputManager {
 
 	public static final String ASK_PURCHASE_AMOUNT_DESC = "구입금액을 입력해 주세요.";
 	public static final String ASK_WINNING_NUMBERS_DESC = "당첨 번호를 입력해 주세요.";
-	public static final String DIGIT_PATTERN_ERROR_MESSAGE = "숫자를 입력 해주세요.";
-	public static final String WINNING_NUMBERS_PATTERN_ERROR_MESSAGE = "당첨 번호 패턴에 매치되지 않습니다";
-	public static final String WINNING_NUMBERS_DUPLICATE_OR_RANGE_ERROR_MESSAGE = "중복된 번호 혹은 범위를 초과한 번호가 있습니다.";
-	public static final String UNIT_ERROR_MESSAGE = TICKET_PRICE + "원 단위로 입력해 해주세요";
-	public static final String DIGIT_PATTERN = "[1-9][0-9]*";
-	public static final String LOTTO_NUMBERS_PATTERN = "^[1-4]*[0-9](,[1-4]*[0-9]){5}$";
-	public static final int ZERO = 0;
 	public static final String SPLIT_DELIMITER = ",";
 	public static final String ASK_BONUS_BALL_DESC = "보너스 볼을 입력해 주세요.";
 	private final Scanner scanner;
@@ -45,13 +37,6 @@ public class InputManager {
 		return Optional.empty();
 	}
 
-	private void validPurchaseAmount(String purchaseAmountInput) {
-		validIsDigit(purchaseAmountInput);
-		if (Integer.parseInt(purchaseAmountInput) % TICKET_PRICE != ZERO) {
-			throw new IllegalArgumentException(UNIT_ERROR_MESSAGE);
-		}
-	}
-
 	public Optional<WinningNumbers> askWiningNumbers() {
 		try {
 			System.out.println(ASK_WINNING_NUMBERS_DESC);
@@ -65,12 +50,6 @@ public class InputManager {
 		return Optional.empty();
 	}
 
-	private void validWiningNumbers(String winningNumbersString) {
-		if (!winningNumbersString.matches(LOTTO_NUMBERS_PATTERN)) {
-			throw new IllegalArgumentException(WINNING_NUMBERS_PATTERN_ERROR_MESSAGE);
-		}
-	}
-
 	static List<Ball> convertWiningNumbers(String winningNumbersString) {
 		String[] numbers = winningNumbersString.split(SPLIT_DELIMITER);
 		validWinningNumbersSize(numbers.length);
@@ -80,12 +59,6 @@ public class InputManager {
 			.distinct()
 			.map(Ball::new)
 			.collect(Collectors.toList());
-	}
-
-	private static void validWinningNumbersSize(int length) {
-		if (length != LottoBalls.LOTTO_NUMBERS_LENGTH) {
-			throw new IllegalArgumentException(WINNING_NUMBERS_DUPLICATE_OR_RANGE_ERROR_MESSAGE);
-		}
 	}
 
 	public Optional<Ball> askBonusBall(WinningNumbers winningNumbers) {
@@ -105,17 +78,5 @@ public class InputManager {
 		int bonusNumber = Integer.parseInt(bonusBallString);
 		validIsDuplication(winningNumbers, bonusNumber);
 		return bonusNumber;
-	}
-
-	private void validIsDuplication(WinningNumbers winningNumbers, int bonusNumber) {
-		if (winningNumbers.containsBallNumber(bonusNumber)) {
-			throw new IllegalArgumentException("이미 포함된 로또 넘버입니다.");
-		}
-	}
-
-	private void validIsDigit(String bonusBallString) {
-		if (!Pattern.matches(DIGIT_PATTERN, bonusBallString)) {
-			throw new IllegalArgumentException(DIGIT_PATTERN_ERROR_MESSAGE);
-		}
 	}
 }
