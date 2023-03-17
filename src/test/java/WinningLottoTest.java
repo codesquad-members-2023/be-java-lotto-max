25050d9b0d389a5cba704b01d6ed559e81c5b377
jwp345 <jwp135@naver.com>
2023-03-18 02:39:19 +0900

    로또 추첨 결과 테스트 코드 작성

--- src/test/java/WinningLottoTest.java
+++ src/test/java/WinningLottoTest.java
@@ -1,19 +1,27 @@
 import kr.codesquad.domain.Lotto;
+import kr.codesquad.domain.LottoPrize;
 import kr.codesquad.domain.WinningLotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
+import static org.assertj.core.api.Assertions.*;
 
 public class WinningLottoTest {
     @Test
     @DisplayName("로또 추첨 결과 테스트")
     void matchLotto_test() {
         WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
-        Lotto lotto = new Lotto(){
-            @Override
-            this.balls =
-        };
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(winningLotto.matchLotto(lotto).get()).isEqualTo(LottoPrize.First);
+        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        assertThat(winningLotto.matchLotto(lotto).get()).isEqualTo(LottoPrize.Second);
+        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+        assertThat(winningLotto.matchLotto(lotto).get()).isEqualTo(LottoPrize.Third);
+        lotto = new Lotto(List.of(1, 2, 3, 4, 10, 11));
+        assertThat(winningLotto.matchLotto(lotto).get()).isEqualTo(LottoPrize.Fourth);
+        lotto = new Lotto(List.of(1, 2, 3, 10, 11, 12));
+        assertThat(winningLotto.matchLotto(lotto).get()).isEqualTo(LottoPrize.Fifth);
     }
+
 }
