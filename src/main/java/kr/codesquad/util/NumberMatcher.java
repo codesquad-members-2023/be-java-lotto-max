c82b4334227e2c1d5ea6a4d7a74e167ab12156f3
O_jeegu <lumirite0@gmail.com>
2023-03-22 04:38:54 +0900

    feat : string 입력값을 숫자로 바꾸어 set에 저장하는 기능 추가

---
+++ src/main/java/kr/codesquad/util/NumberMatcher.java
@@ -0,0 +1,15 @@
+package kr.codesquad.util;
+
+import java.util.*;
+
+public class NumberMatcher {
+
+    public Set<Integer> storeWinningNums(String winningNums) {
+        Set<Integer> winningNumList = new HashSet<>();
+        String[] nums = winningNums.split(",");
+        for(String num : nums) {
+            winningNumList.add(Integer.valueOf(num.trim()));
+        }
+        return winningNumList;
+    }
+}
